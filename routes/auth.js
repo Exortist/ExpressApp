1671787081e31441ffd4060817ed682047446153
Dmitriy <megagugl530@gmail.com>
2020-09-08 21:10:26 +0300

    Asynchronous validators

--- routes/auth.js
+++ routes/auth.js
@@ -70,33 +70,28 @@ router.post('/login', async (req, res) => {
 router.post('/register', registersValidators, async (req, res) => {
 
     try {
-        const {email, password, confirm, name} = req.body
-
-        const candidate = await User.findOne({email})
+        const {email, password, name} = req.body
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             req.flash('registerError', errors.array()[0].msg)
             return res.status(422).redirect('/auth/login#register')
         }
 
-        if (candidate) {
-            req.flash('registerError', 'Пользователь с таким email уже существует')
-            res.redirect('/auth/login#register')
-        } else {
-            const hashPassword = await bcrypt.hash(password, 10)
-            const user = new User({
-                email, name, password: hashPassword, cart: {items: []}
-            })
-            await user.save()
-            await client.sendMail(regEmail(email), (err, info) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log('Message sent: ' + info.message);
-                }
-            })
-            res.redirect('/auth/login#login')
-        }
+
+        const hashPassword = await bcrypt.hash(password, 10)
+        const user = new User({
+            email, name, password: hashPassword, cart: {items: []}
+        })
+        await user.save()
+        await client.sendMail(regEmail(email), (err, info) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Message sent: ' + info.message);
+            }
+        })
+        res.redirect('/auth/login#login')
+
 
     } catch (e) {
         console.log("Ошибка", e)
--- utils/validators.js
+++ utils/validators.js
@@ -1,7 +1,17 @@
 const {body} = require('express-validator/check')
-
+const User = require('../models/user')
 exports.registersValidators = [
-    body('email').isEmail().withMessage('Введите корректный Email'),
+    body('email').isEmail().withMessage('Введите корректный Email').custom(async (value, {req}) => {
+        try {
+            const user = await User.findOne({email: value})
+
+            if (user) {
+                return Promise.reject('Такой email уже занят')
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }),
     body('password', 'Пароль должен содержать минимум 6 символов').isLength({min: 6, max: 56}).isAlphanumeric(),
     body('confirm').custom((value, {req}) => {
         if (value !== req.body.password) {
